716bc05b6d436801715b59f0669e27785bbaf03b
JuliaTodorova <juliastoycheva@gmail.com>
2016-04-16 10:25:39 -0700

    Created class variables

--- Hangman.java
+++ Hangman.java
@@ -3,12 +3,17 @@ package hangman;
 import java.util.Scanner;
 import javax.swing.JFrame;
 import java.util.Random;
+import java.io.CharArrayWriter;
 
 /**
  *
  * @author julkata
  */
 public class Hangman {
-    
+
+    protected char[] word = null;
+    protected CharArrayWriter correctLetters;
+    protected int lives = 6;
+    protected HangmanCanvas canvas;
 
 }
