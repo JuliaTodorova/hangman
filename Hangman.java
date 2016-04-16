0e593a408369368e2cf71baee55e99ba8fadf836
JuliaTodorova <juliastoycheva@gmail.com>
2016-04-16 01:05:47 -0700

    initial commit

---
+++ Hangman.java
@@ -0,0 +1,14 @@
+package hangman;
+
+import java.util.Scanner;
+import javax.swing.JFrame;
+import java.util.Random;
+
+/**
+ *
+ * @author julkata
+ */
+public class Hangman {
+    
+
+}
---
+++ HangmanCanvas.java
@@ -0,0 +1,13 @@
+package hangman;
+
+import java.awt.*;
+import static java.awt.Font.PLAIN;
+
+/**
+ *
+ * @author julkata
+ */
+public class HangmanCanvas extends Canvas {
+    
+
+}
